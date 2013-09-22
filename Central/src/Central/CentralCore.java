5fc0208c4f386bafc2971b0b633c63dbf65b921c
squarepp2 <ashida6@gmail.com>
2013-09-22 17:39:46 +0900

    うｐうｐ

--- Central/src/Central/CentralCore.java
+++ Central/src/Central/CentralCore.java
@@ -1,17 +1,24 @@
 package Central;
 
 import java.io.File;
+import java.util.logging.Logger;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import Central.Configuration.ConfigurationManager;
+
 public class CentralCore extends JavaPlugin {
 	public static CentralCore Instance;
+	public static Logger log;
+	private static ConfigurationManager cfg;
 	public CentralCore(){
 		Instance = this;
+		log = this.getLogger();
 	}
 	@Override
 	public void onEnable(){
-		
+		cfg = new ConfigurationManager(this);
+		log.info("Enabled " + this.getDescription().getName() + "!");
 	}
 	@Override
 	public void onDisable(){
